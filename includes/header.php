95b83d35b3d3e0529e4707bb63af784ff62540fc
Andrew <andrew.asada@gmail.com>
2017-08-13 17:00:00 -0700

    Test

--- includes/header.php
+++ includes/header.php
@@ -33,7 +33,7 @@
                 <a class="nav-link" href="categories.php">Categories</a>
               </li>
               <li class="nav-item <?php if($curdir == "search.php"){echo "active";} ?>">
-                <a class="nav-link" href="categories.php">Quick Search</a>
+                <a class="nav-link" href="categories.php">Quick Searchs</a>
               </li>
             </ul>
           </div>
