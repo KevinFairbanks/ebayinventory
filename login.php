8833836566595bdfb2cf8eb46c34291aaa50703c
Andrew <andrew.asada@gmail.com>
2017-08-25 20:21:20 -0700

    edits

--- login.php
+++ login.php
@@ -16,9 +16,12 @@
                 // Authentication successful - Set session
 
                 $_SESSION['auth'] = 1;
-                setcookie("username", $_POST['name'], time()+(84600*30));
-                header('Location: index.php');
-                exit;
+                if( isset($_SESSION['auth']) == 1){
+                    setcookie("username", $_POST['name'], time()+(84600*30));
+                    header('Location: index.php');
+                    exit;
+                }
+
             }else {
                 echo "ERROR: Incorrect username or password!";
             }
