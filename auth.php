d51858ecf5af803ba44ede83bf1894470c59d01a
Andrew <andrew.asada@gmail.com>
2017-08-25 20:52:03 -0700

    moved code around

---
+++ auth.php
@@ -0,0 +1,29 @@
+<?php session_start(); ?>
+    <?php
+        // GET SOME VARIABLES
+        $name = $_POST['name'];
+        $pass = $_POST['pass'];
+
+        if( isset($name) || isset($pass) ){
+            if( empty($name) ) {
+                header('Location: login.php');
+            }
+            if( empty($pass) ) {
+                header('Location: login.php');
+            }
+            if( $name == "ebay" && $pass == "ebay" ){
+                // Authentication successful - Set session
+
+                $_SESSION['auth'] = 1;
+                setcookie("username", $_POST['name'], time()+(84600*30));
+                header('Location: index.php');
+                // exit;
+
+            }else {
+                echo "ERROR: Incorrect username or password!";
+            }
+        } else {
+
+        }
+
+    ?>
--- login.php
+++ login.php
@@ -1,51 +1,12 @@
-<?php session_start(); ?>
-<?php
-    if($_SESSION['auth'] == 1){
-        setcookie("username", $_POST['name'], time()+(84600*30));
-        header('Location: index.php');
-    }
-?>
+
 <?php include('includes/header.php'); ?>
-<?php if( isset( $_POST['submit'] ) ): ?>
-    <?php
-        // GET SOME VARIABLES
-        $name = $_POST['name'];
-        $pass = $_POST['pass'];
-
-        if( isset($name) || isset($pass) ){
-            if( empty($name) ) {
-                // header('Location: login.php');
-            }
-            if( empty($pass) ) {
-                // header('Location: login.php');
-            }
-            if( $name == "ebay" && $pass == "ebay!_x" ){
-                // Authentication successful - Set session
-
-                $_SESSION['auth'] = 1;
-                // setcookie("username", $_POST['name'], time()+(84600*30));
-                // header('Location: index.php');
-                // exit;
-
-            }else {
-                echo "ERROR: Incorrect username or password!";
-            }
-        } else {
-
-        }
-
-    ?>
-
-<?php else: ?>
-    <!-- enter some shit in already -->
-<?php endif; ?>
 
 <div class="container">
     <div class="row justify-content-center">
     <div class="col-lg-4">
         <div class="login">
             <img src="img/ebay-logo.png" alt="">
-            <form method="post" action="/login.php">
+            <form method="post" action="auth.php">
                 <div class="form-group">
                     <label for="">Username:</label>
                     <input type="text" name="name" value="" class='form-control'>
@@ -55,7 +16,6 @@
                     <input type="password" name="pass" class='form-control'>
                 </div>
                 <input type="submit" name="submit" value="Log In" class='btn btn-success'>
-
             </form>
         </div>
     </div>
